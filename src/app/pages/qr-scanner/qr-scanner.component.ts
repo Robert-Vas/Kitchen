67f816db05cade2061330e88f8ad1cc138198f63
Robert <vvasquez@memphis.edu>
2021-11-19 23:51:10 -0600

    check: not say done

--- src/app/pages/qr-scanner/qr-scanner.component.ts
+++ src/app/pages/qr-scanner/qr-scanner.component.ts
@@ -44,7 +44,7 @@ export class QrScannerComponent implements OnInit {
   }
 
   private markOrderComplete() {
-    this.order.status = OrderStatus.Done;
+    //this.order.status = OrderStatus.Done;
     this.orderSubscription.unsubscribe();
     this.orderService.editOrder(this.order, this.order.id);
     this.showSuccessMessage();
@@ -65,6 +65,7 @@ export class QrScannerComponent implements OnInit {
       }
     }); 
   }
+  
 
   private checkStatus() {
     if (this.order.status != OrderStatus.Start && this.order.status != OrderStatus.Ready) {
